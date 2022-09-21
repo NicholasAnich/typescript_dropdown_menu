c9c4af1a747f3606453dd0d6b57fee538b79fbee
Nicholas Anich <NicholasAnich.infinitevolume@gmail.com>
2022-09-20 21:19:27 -0700

    simply changed option label names in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import { useState } from "react";
 import { Select, SelectOption } from "./Select";
 
 const options = [
-  { label: "First", value: 1 },
-  { label: "Second", value: 2 },
-  { label: "Third", value: 3 },
-  { label: "Fourth", value: 4 },
-  { label: "Fith", value: 5 },
-  { label: "Sixth", value: 6 },
-  { label: "Seventh", value: 7 },
+  { label: "Javascript", value: 1 },
+  { label: "HTML", value: 2 },
+  { label: "CSS", value: 3 },
+  { label: "React", value: 4 },
+  { label: "Python", value: 5 },
+  { label: "MySQL", value: 6 },
+  { label: "NextJS", value: 7 },
 ];
 
 function App() {
