eaaeaac062b3c58859370f50d5b9555f3df528f1
Nicholas Anich <NicholasAnich.infinitevolume@gmail.com>
2022-09-20 21:13:07 -0700

    Implemented single and mutliple drop down menus

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { Select, SelectOption } from "./Select";
+
+const options = [
+  { label: "First", value: 1 },
+  { label: "Second", value: 2 },
+  { label: "Third", value: 3 },
+  { label: "Fourth", value: 4 },
+  { label: "Fith", value: 5 },
+  { label: "Sixth", value: 6 },
+  { label: "Seventh", value: 7 },
+];
+
+function App() {
+  const [value1, setValue1] = useState<SelectOption[]>([options[0]]);
+  const [value2, setValue2] = useState<SelectOption | undefined>(options[0]);
+
+  return (
+    <div>
+      <Select
+        multiple
+        options={options}
+        value={value1}
+        onChange={(o) => setValue1(o)}
+      />
+      <br />
+      <Select options={options} value={value2} onChange={(o) => setValue2(o)} />
+    </div>
+  );
+}
+
+export default App;
