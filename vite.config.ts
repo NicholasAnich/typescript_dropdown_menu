a7f9439a6b8129584a52f76824b09097f1a734a3
Nicholas Anich <NicholasAnich.infinitevolume@gmail.com>
2022-09-20 23:51:13 -0700

    adding dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  //Base url to website
+  base: '/multiple_dropDown_menu/',
   plugins: [react()]
 })
